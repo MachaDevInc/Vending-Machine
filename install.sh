2e29f4146646af7e4c0ccab7352423477c0935b7
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-02-22 23:59:26 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-output=$(cat /root/disk_partition_flag)
+output=$(cat /root/install_step)
 if [ [ "$output" == 0 ] || [ "$output" == "" ] ]; then
     echo "Partitioning the memory and setting up swap memory"
     opkg update
@@ -20,7 +20,7 @@ EOF
     /etc/init.d/fstab enable
     block detect > /etc/config/fstab
 
-    flag_update=/root/disk_partition_flag
+    flag_update=/root/install_step
     cat /root/flag_one>$flag_update
 
     reboot && exit
@@ -29,19 +29,19 @@ fi
 if [ "$output" == 1 ]; then
     echo "Downloading the packages and libraries"
     opkg update
-    opkg install python3-pip git-http
+    opkg install python3-pip
 
     pip3 install --upgrade pip
     pip3 install stripe
-
-    cd
-
-    git clone https://github.com/MachaDevInc/Vending-Machine.git
+    pip3 install python-dotenv
+    pip3 install pyserial
 
     cp /root/Vending-Machine/* /root/
 
     rm -r /root/Vending-Machine/
 
-    flag_update=/root/disk_partition_flag
+    cp /root/rclocal /etc/rc.local
+
+    flag_update=/root/install_step
     cat /root/flag_two>$flag_update
 fi
