bef12b1f70a3e1279f35a3f36db14bb8d7cd2a18
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-02-27 22:12:03 +0500

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -5,8 +5,8 @@ swapon /dev/mmcblk0p1
 
 output=$(cat /root/install_step)
 if [ "$output" != 2 ]; then
-    sh /root/install.sh
+    sh /root/install.sh >> /root/output.txt 2>&1
 else
-    python3 /root/WiFiSetup.py
-    python3 /root/VM.py
+    python3 /root/WiFiSetup.py >> /root/output.txt 2>&1
+    python3 /root/VM.py >> /root/output.txt 2>&1
 fi
