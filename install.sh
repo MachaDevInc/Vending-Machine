4b63d69e7b46f54a87fd376169ae5c9b94e33dc3
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-03-01 00:25:06 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,11 +20,11 @@ EOF
     /etc/init.d/fstab enable
     block detect > /etc/config/fstab
 
-    mount /dev/mmcblk0p2 /root
+    mount /dev/mmcblk0p2 /overlay
 
     sed -i "11s+0+1+" /etc/config/fstab
     sed -i "16s+0+1+" /etc/config/fstab
-    sed -i "s+'/mnt/mmcblk0p2'+'/root'+g" /etc/config/fstab
+    sed -i "s+'/mnt/mmcblk0p2'+'/overlay'+g" /etc/config/fstab
 
     cd
 
